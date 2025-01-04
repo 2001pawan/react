620c20cc85c6e896e7e061f0e6f3ae6d423a3751
Pawan Gurung <pawan.grgn@gmail.com>
2025-01-04 21:07:24 +0545

    without undo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import './App.css';
+
 
 function Square({ value, onSquareClick }) {
   return (
@@ -9,16 +11,47 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext,setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
+  function resetGame() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
   }
 
+  function handleClick(i) {
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if(xIsNext){
+      nextSquares[i] = 'X';
+    }
+    else{
+      nextSquares[i] = 'O';
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+
+  }
+
+  const winner = calculateWinner(squares);
+
+  let gameStatus;
+    if(winner){
+      gameStatus="winner:"+winner;
+    }
+    else{
+      gameStatus="next turn:"+(xIsNext? 'X': 'O');
+    }
+
+  
   return (
     <>
+      <div>{gameStatus}</div>
+      <button className="reset" onClick={resetGame}>Restart</button>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -37,3 +70,22 @@ export default function Board() {
     </>
   );
 }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+} 
